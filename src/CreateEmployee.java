f0b5d368f8aebad6d3f93d36fee8291eab50e2e6
sudarsh <sudarsh@bangthetable.com>
2015-03-14 01:40:03 +0530

    Fixed test cases

--- src/CreateEmployee.java
+++ src/CreateEmployee.java
@@ -7,6 +7,7 @@ import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import java.util.Random;
 
 public class CreateEmployee {
     private WebDriver driver;
@@ -43,15 +44,19 @@ public class CreateEmployee {
         assertEquals("Add New Employee", driver.findElement(By.cssSelector("h3")).getText());
         assertEquals("Personal Information", driver.findElement(By.cssSelector("h4")).getText());
         driver.findElement(By.id("employee_email")).clear();
-        driver.findElement(By.id("employee_email")).sendKeys("emp122221213@gmail.com");
+
+        Random randomGenerator = new Random();
+        int empNum = randomGenerator.nextInt(100);
+
+        driver.findElement(By.id("employee_email")).sendKeys("emp_" + empNum + "@gmail.com");
         driver.findElement(By.id("employee_first_name")).clear();
-        driver.findElement(By.id("employee_first_name")).sendKeys("emp12312231111");
-        driver.findElement(By.id("employee_last_name")).clear();
-        driver.findElement(By.id("employee_last_name")).sendKeys("last12312231111");
+        driver.findElement(By.id("employee_first_name")).sendKeys("emp_" + empNum );
+                driver.findElement(By.id("employee_last_name")).clear();
+        driver.findElement(By.id("employee_last_name")).sendKeys("last_name");
         driver.findElement(By.name("commit")).click();
         assertEquals("Employer Related Information", driver.findElement(By.xpath("//form[@id='new_employee']/feildset/div[2]/h3")).getText());
         driver.findElement(By.id("employee_employee_number")).clear();
-        driver.findElement(By.id("employee_employee_number")).sendKeys("1009131111");
+        driver.findElement(By.id("employee_employee_number")).sendKeys(String.valueOf(empNum));
         driver.findElement(By.name("commit")).click();
         assertEquals("AAPC Member Details", driver.findElement(By.xpath("//form[@id='new_employee']/feildset/div[3]/div/h3")).getText());
         driver.findElement(By.name("commit")).click();
@@ -60,14 +65,14 @@ public class CreateEmployee {
         driver.findElement(By.name("commit")).click();
         for (int second = 0;; second++) {
             if (second >= 60) fail("timeout");
-            try { if (isElementPresent(By.linkText("emp12312231111"))) break; } catch (Exception e) {}
+            try { if (isElementPresent(By.linkText("emp_" + empNum ))) break; } catch (Exception e) {}
             Thread.sleep(1000);
         }
 
-        assertEquals("×Employee emp12312231111 was created", driver.findElement(By.xpath("//div[2]/div/div/div")).getText());
-        driver.findElement(By.linkText("Sam Smith")).click();
+        assertEquals("×\nEmployee emp_" + empNum + " was created", driver.findElement(By.xpath("//div[2]/div/div/div")).getText());
+                driver.findElement(By.linkText("Sam Smith")).click();
         driver.findElement(By.linkText("Logout")).click();
-        assertEquals("×Signed out successfully.", driver.findElement(By.xpath("//div[2]/div/div/div")).getText());
+        assertEquals("×\nSigned out successfully.", driver.findElement(By.xpath("//div[2]/div/div/div")).getText());
     }
 
     @After
